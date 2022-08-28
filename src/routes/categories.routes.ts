8547b8f6116669267bb99b84e6285d43bb1397a8
Júnior Nunes <denisonprobable13@gmail.com>
2022-08-28 17:06:48 -0300

    feat: verifying  if name already exists

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,5 @@
-import { Router } from "express";
+import { response, Router } from "express";
 
-import { Category } from "../model/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
@@ -9,9 +8,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: "Category Already exists!" });
+  }
+
   categoriesRepository.create({ name, description });
 
-  return res.status(201).json({ categoriesRepository });
+  return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+
+  return res.json({ all });
 });
 
 export { categoriesRoutes };
