20d6c48db8573175d10851d47203c9f069d41924
Júnior Nunes <denisonprobable13@gmail.com>
2022-09-07 20:53:53 -0300

    feat: uploading archives with multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,27 @@
 import { response, Router } from "express";
+import multer from "multer";
 import { CategoriesRepository } from "../modules/cars/repositories/implementations/CategoriesRepository";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = CategoriesRepository.getInstance();
-
-categoriesRoutes.post("/", (req, res) => {
-  return createCategoryController.handle(req, res);
+const upload = multer({
+  dest: "./tmp",
 });
 
-categoriesRoutes.get("/", (req, res) => {
-  return listCategoriesController.handle(req, res);
+categoriesRoutes.post("/", (req, res) =>
+  createCategoryController.handle(req, res)
+);
+
+categoriesRoutes.get("/", (req, res) =>
+  listCategoriesController.handle(req, res)
+);
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+  console.log(file);
+  return res.send();
 });
 
 export { categoriesRoutes };
