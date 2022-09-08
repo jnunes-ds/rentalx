306c86ccf4892e0a6aea95c6f1baaa9738237b2c
Júnior Nunes <denisonprobable13@gmail.com>
2022-09-07 23:20:26 -0300

    feat: getting to know the stream concept

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,20 @@
-import { Express } from "express";
+import fs from "fs";
+import { parse as csvParse } from "csv-parse";
 
 interface MulterFile extends Express.Multer.File {}
 
 class ImportCategoryUseCase {
   execute(file: MulterFile): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    // @ts-ignore
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
